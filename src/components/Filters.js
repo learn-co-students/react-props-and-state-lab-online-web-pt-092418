b2e510b8afc3d47ff7097f83ae27450e3cf33fc4
github username <github email address>
2019-06-19 13:37:24 -0400

    wip - selection callback function.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+
+
 class Filters extends React.Component {
+
+  selection = (event) => {
+    debugger
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select onChange={this.selection} name="type" id="type">
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +22,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" >Find pets</button>
         </div>
       </div>
     )
