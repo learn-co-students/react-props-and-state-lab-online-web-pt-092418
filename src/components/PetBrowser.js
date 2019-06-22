cc8855d80099f846d47509af734dde5a9aced6af
github username <github email address>
2019-06-21 23:03:24 -0400

    change {} to () for map method in render function.

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,11 +4,9 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
-      debugger
-    const petCards = this.props.pets.map(pet => {
+    const petCards = this.props.pets.map(pet => (
       <Pet pet={pet} onAdoptPet={this.props.onAdoptPet} key={pet.id}/>
-    });
-
+    ));
     return  <div className="ui cards">{petCards}</div>
   }
 }
